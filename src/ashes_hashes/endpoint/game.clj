4d158631260d4da7f213ad83eba6916166a66fb5
Dan Brook <dan@broquaint.com>
2016-02-13 16:50:30 +0000

    A functioning enlive based template
    So taking the data that was put in ES by the logsumer, pull it out in
    the game endpoint and apply it to a template. Nice'n'simple.

--- src/ashes_hashes/endpoint/game.clj
+++ src/ashes_hashes/endpoint/game.clj
@@ -1,14 +1,27 @@
 (ns ashes-hashes.endpoint.game
   (:require [compojure.core :refer :all]
             [hiccup.core :as h]
-            [clojurewerkz.elastisch.rest.document :as esd]))
+            [net.cgrand.enlive-html :as html]
+            [clojurewerkz.elastisch.rest.document :as esd]
+            [clojurewerkz.elastisch.rest.response :as esrsp]))
+
+(defn game-to-str [doc]
+  (str (:player_name doc) " the " (:charabbrev doc) " got " (:score doc)))
+
+(def the-template "ashes_hashes/endpoint/game.html")
+
+(html/defsnippet game-entry the-template  [:.game]
+  [game]
+  [:li] (html/content (game-to-str game)))
+
+(html/deftemplate games the-template
+  [games]
+  [:.game] (html/content (map game-entry games)))
 
 (defn show-game [config]
-  (let [result (esd/get (:conn (:es config)) "scratch" "game" "AVDh6aR1nw9nrGaSIT86")
-        doc (:_source result)]
-    (h/html
-     [:h1 (:character doc) " the " (:background doc) " got " (:score doc)]
-     "\n")))
+  (let [result (esd/search (:conn (:es config)) "scratch" "game")
+        hits (esrsp/hits-from result)]
+    (games (map :_source hits))))
 
 (defn game-endpoint [config]
   (routes
